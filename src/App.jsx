162f9539ecbb8cf82bf9a9a93b0cd502f5acba59
hichem <hichembenanififa@gmail.com>
2025-10-29 10:55:04 +0100

    Fix blank page: import React for ErrorBoundary class

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { supabase } from './config/supabaseClient'
 import { USE_SUPABASE } from './config'
 import { DataProvider } from './context/UnifiedDataContext'
